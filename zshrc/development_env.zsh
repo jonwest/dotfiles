4f337ce4979a41cfefb27d89f208fa5aff9d4adf
Jon West <jon@jw.gs>
2022-01-25 15:49:51 -0800

    fix: Speed up slow NVM

--- zshrc/development_env.zsh
+++ zshrc/development_env.zsh
@@ -13,6 +13,35 @@ export NVM_LAZY_LOAD=true
 export NVM_AUTO_USE=true
 export NVM_COMPLETION=true
 
+lazynvm() {
+  unset -f nvm node npm npx
+  export NVM_DIR=~/.nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  if [ -f "$NVM_DIR/bash_completion" ]; then
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+  fi
+}
+
+nvm() {
+  lazynvm 
+  nvm $@
+}
+ 
+node() {
+  lazynvm
+  node $@
+}
+ 
+npm() {
+  lazynvm
+  npm $@
+}
+
+npx() {
+  lazynvm
+  npx $@
+}
+
 ####
 ## Ruby
 ####
