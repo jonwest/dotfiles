a9bfc7462b45a2ce9b1f4136ac2009311fdb9b6b
Jon West <jon@jw.gs>
2022-03-15 23:27:33 -0700

    fix(): Don't use exa on Linux

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -7,8 +7,13 @@
 
 alias vim="nvim"  # Use neovim instead of vim
 
-alias lt="exa -l -a -h --git --icons --color never -T -L 2"     # Show folder tree
-alias ll="exa -l -a -h --git --icons --color never"             # `ls` with icons
+if [[ $(uname) = Darwin ]]; then
+  alias lt="exa -l -a -h --git --icons --color never -T -L 2"     # Show folder tree
+  alias ll="exa -l -a -h --git --icons --color never"             # `ls` with icons
+else
+  alias lt="ls -alh"
+  alias ll="ls -alh"
+fi
 
 ####
 ## GIT
@@ -17,25 +22,3 @@ alias gc="git commit -m"
 alias gco="git checkout"
 alias gcob="git checkout -b"
 
-
-# Download v.reddit videos
-function vred {
-    if [[ -z "$1" ]]; then
-        echo "Usage: \n\t vred (url)"
-        echo "\t Will download to current working folder"
-        return 0
-    fi
-
-    $HOME/bin/vred --maxquality -u $1 -p $(pwd)
-}
-
-# Download YouTube as audio
-function ytdl {
-    if [[ -z "$1" ]]; then
-        echo "Usage: \n\t ytdl (url)"
-        echo "\t Will download to current working folder"
-        return 0
-    fi
-
-    youtube-dl -x --audio-format mp3 --prefer-ffmpeg "${1}"
-}
