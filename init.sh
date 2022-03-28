5c80d4a5c13a6e62c3e5efb657d14926e36f6b9d
Jon West <jon@jw.gs>
2022-03-28 08:55:28 -0700

    fix(init): Remove shell change

--- init.sh
+++ init.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Change to zsh if available
-if [[ ! $(echo $SHELL | awk '/zsh/') ]]; then
-  echo "Shell is not set to ZSH"
-  if [[ $(which zsh) ]]; then
-    if [[ ${REMOTE_CONTAINERS} ]]; then
-      # Use sudo to chsh since we won't know the user password in a container
-      sudo chsh -s $(which zsh) $(whoami)
-    else 
-      chsh -s $(which zsh) $(whoami)
-    fi
-
-    echo "Shell has been set to zsh";
-  else
-    echo "zsh is not present, cannot change shell."
-  fi
-fi
-
 # Require starship
 if [[ ! $(which starship) ]]; then
   curl -sS https://starship.rs/install.sh | sh
