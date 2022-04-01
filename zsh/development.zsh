770cf319b2748fe0150f72974d165555c8dd8f4a
Jon West <jon@jw.gs>
2022-04-01 10:09:49 -0700

    feat(development): Add rbenv to path

--- zsh/development.zsh
+++ zsh/development.zsh
@@ -4,3 +4,5 @@
 
 # ZSH completion for Kubernetes
 [[ $(which kubectl) ]] && source <(kubectl completion zsh)
+
+[[ $(which rbenv) ]] && eval "$(rbenv init - zsh)" && export PATH=~/.rbenv/shims:$PATH
