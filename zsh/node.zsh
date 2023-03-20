808a5dde1118a31231ac2b01ca13b82e12af630c
Jon West <jon@jw.gs>
2023-03-20 09:37:50 -0700

    fix: remove node bash completion

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -2,9 +2,6 @@ lazynvm() {
   unset -f nvm node npm npx
   export NVM_DIR=${HOME}/.nvm
    [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
-  if [ -f "${NVM_DIR}/etc/bash_completion.d/nvm" ]; then
-    [ -s "${NVM_DIR}/etc/bash_completion.d/nvm" ] && \. "${NVM_DIR}/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-  fi
 }
 
 nvm() {
