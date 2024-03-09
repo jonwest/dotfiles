98d8c135b359163e812ea774df8b1480bd55f76a
Jon West <jon@jw.gs>
2024-03-09 13:36:00 -0800

    fix: lazygit install was broken

--- init.sh
+++ init.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+TRACE="${TRACE:-0}";
+set -o errexit;
+set -o nounset;
+set -o pipefail;
+if [[ -n "${TRACE}" && "${TRACE}" -ge 2 ]]; then set -o xtrace; fi
 
 export DOTFILES_INSTALLER="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Require starship
 if ! command -v starship >/dev/null 2>&1; then
-  curl -sS https://starship.rs/install.sh | sh -s -- -y
+  curl -sS https://starship.rs/install.sh | sh -s -- -y;
 fi
 
 # Install lazygit
 if ! command -v lazygit >/dev/null 2>&1; then
-  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  pushd /tmp || echo "/tmp doesn't exist" && exit 1;
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*');
+  cd /tmp;
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_$(uname)_$(uname -m).tar.gz";
   tar xf lazygit.tar.gz lazygit;
   mv lazygit "${HOME}/bin/lazygit";
+  cd -;
 fi
 
 
@@ -26,5 +32,3 @@ fi
 [[ $(uname) = Darwin ]] && source ./mac/mac-setup.sh
 
 unset DOTFILES_INSTALLER
-
-
