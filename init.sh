610e7c41a978d7ce9a8814c2ea0a750a80f453b3
Jon West <jon@jw.gs>
2024-03-09 13:18:55 -0800

    feat: add lazygit

--- init.sh
+++ init.sh
@@ -7,6 +7,16 @@ if ! command -v starship >/dev/null 2>&1; then
   curl -sS https://starship.rs/install.sh | sh -s -- -y
 fi
 
+# Install lazygit
+if ! command -v lazygit >/dev/null 2>&1; then
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  pushd /tmp || echo "/tmp doesn't exist" && exit 1;
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_$(uname)_$(uname -m).tar.gz";
+  tar xf lazygit.tar.gz lazygit;
+  mv lazygit "${HOME}/bin/lazygit";
+fi
+
+
 # Set up NvChad if Neovim is available
 if [[ $(which nvim) ]]; then
   nvim -c 'hi NormalFloat guibg=#1e222a' -c PackerSync -c "qa!"
