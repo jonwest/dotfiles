66378cf9ad831969d6774242d325a660c62d80cb
Jon West <jon@jw.gs>
2024-03-22 14:38:50 -0700

    chore: formatting

--- mac/mac-setup.sh
+++ mac/mac-setup.sh
@@ -16,7 +16,7 @@
 
 
 # Fail fast
-set -euo pipefail
+set -euo pipefail;
 
 include() {
   [[ -f "$1" ]] && source "$1" || echo "ERROR loading ${1}"
@@ -24,18 +24,18 @@ include() {
 
 # If this is run directly, and not through the installer, DOTFILES_FOLDER won't be set,
 # so use the DOTFILES_INSTALLER variable in its place.
-[[ -z ${DOTFILES_INSTALLER+x} ]] && DOTFILES_INSTALLER=${DOTFILES_FOLDER}
+[[ -z ${DOTFILES_INSTALLER+x} ]] && DOTFILES_INSTALLER="${DOTFILES_FOLDER}";
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
+osascript -e 'tell application "System Preferences" to quit';
 
-include "${DOTFILES_INSTALLER}/mac/input-preferences.sh"
-include "${DOTFILES_INSTALLER}/mac/finder-preferences.sh"
-include "${DOTFILES_INSTALLER}/mac/dock-preferences.sh"
-include "${DOTFILES_INSTALLER}/mac/misc-preferences.sh"
-include "${DOTFILES_INSTALLER}/mac/software-installation.sh"
-include "${DOTFILES_INSTALLER}/mac/dock-remove-bloat.sh"
+include "${DOTFILES_INSTALLER}/mac/input-preferences.sh";
+include "${DOTFILES_INSTALLER}/mac/finder-preferences.sh";
+include "${DOTFILES_INSTALLER}/mac/dock-preferences.sh";
+include "${DOTFILES_INSTALLER}/mac/misc-preferences.sh";
+include "${DOTFILES_INSTALLER}/mac/software-installation.sh";
+include "${DOTFILES_INSTALLER}/mac/dock-remove-bloat.sh";
 
 # Unset failing fast
-set +euo pipefail
+set +euo pipefail;
