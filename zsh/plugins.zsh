d7f239fd8a0ab4ef8e8459a58facc833bae3308d
Jon West <jon@jw.gs>
2024-03-22 13:08:33 -0700

    feat: bring zinit into repo

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,15 +6,18 @@
 # ┌────────────────────────┐
 # │  ZINIT INITIALIZATION  │
 # └────────────────────────┘
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+export ZINIT[HOME_DIR]="${ZSH_FOLDER}/zinit"
+
+if [[ ! -f "${ZINIT[HOME_DIR]}/zinit.git/zinit.zsh" ]]; then
   print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-  command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-  command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+  command mkdir -p "${ZINIT[HOME_DIR]}" && command chmod g-rwX "${ZINIT[HOME_DIR]}"
+  command git clone https://github.com/zdharma-continuum/zinit "${ZINIT[HOME_DIR]}/zinit.git" && \
     print -P "%F{33} %F{34}Installation successful.%f%b" || \
     print -P "%F{160} The clone has failed.%f%b"
 fi
 
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+source "${ZINIT[HOME_DIR]}/zinit.git/zinit.zsh";
+
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
