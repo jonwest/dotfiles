7261ff9d6f899d87340858648f17b04028f61b28
Jon West <jon@jw.gs>
2024-05-23 10:15:47 -0700

    fix: update lazynvm unset positions

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,33 +1,38 @@
 lazynvm() {
-  unset -f nvm node npm npx
   export NVM_DIR=${HOME}/.nvm
-   [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
+
+  [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
   if [ -f "${NVM_DIR}/etc/bash_completion.d/nvm" ]; then
     [ -s "${NVM_DIR}/etc/bash_completion.d/nvm" ] && \. "${NVM_DIR}/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
   fi
 }
 
 nvm() {
+  unset -f nvm
   lazynvm 
   nvm $@
 }
 
 node() {
+  unset -f node
   lazynvm
   node $@
 }
 
 npm() {
+  unset -f npm
   lazynvm
   npm $@
 }
 
 npx() {
+  unset -f npx
   lazynvm
   npx $@
 }
 
 yarn() {
+  unset -f yarn
   lazynvm
   yarn $@
 }
